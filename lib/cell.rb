1da941f44e2a80b886a4732b86b0fea46b2e4c85
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-09 15:54:05 -0600

    Create place_ship method and refactor ship method
    Co-Authored-By: Ana Perez Santos <aperezsantos@users.noreply.github.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,15 +1,16 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate_parameter)
     @coordinate = coordinate_parameter
-  end
-
-  def ship
-    nil
+    @ship = nil
   end
 
   def empty?
     true
+  end
+
+  def place_ship(ship_object_parameter)
+    @ship = ship_object_parameter
   end
 end
