4b95431b0eaa172651aed7a93e2f62fc46755409
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-09 15:00:48 -0600

    Create cell test and class and its initialize method
    Co-Authored-By: Ana Perez Santos <aperezsantos@users.noreply.github.com>

---
+++ lib/cell.rb
@@ -0,0 +1,6 @@
+class Cell
+
+  def initialize(coordinate_parameter)
+    @coordinate = coordinate_parameter
+  end
+end
---
+++ test/cell_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new("B4")
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+end
