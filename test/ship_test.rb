44a9d2262d6bea0cd27da0a46120863afc02eed3
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-08 20:47:30 -0600

    Add tests for health and hit
    Co-Authored-By: Ana Perez Santos <aperezsantos@users.noreply.github.com>

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -19,4 +19,33 @@ class ShipTest < Minitest::Test
     assert_equals "Submarine", @ship_2.name
     assert_equals 2, @ship_2.length
   end
+
+  # @health
+  def test_health_equals_length
+    assert_equals @ship.length, @ship.health
+    assert_equals @ship_2.length, @ship_2.health
+  end
+
+  # .hit
+  def test_hit_decreases_health_by_one
+
+    assert_equals 3, @ship.health
+    @ship.hit
+    assert_equals 2, @ship.health
+    @ship.hit
+    assert_equals 1, @ship.health
+    @ship.hit
+    assert_equals 0, @ship.health
+    # Optional last assertion to ensure health can't go below zero:
+    # @ship.hit
+    # assert_equals 0, @ship.health
+
+    assert_equals 2, @ship_2.health
+    @ship_2.hit
+    assert_equals 1, @ship_2.health
+    @ship_2.hit
+    assert_equals 0, @ship_2.health
+  end
+
+  # .sunk?
 end
