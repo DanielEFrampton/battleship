3c0c5d1f6518a18263f8c161aa807d1108ed4ee4
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-09 20:06:52 -0600

    Add and reformat comments throughout test file

--- test/board_test.rb
+++ test/board_test.rb
@@ -17,24 +17,41 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_starts_with_hash_of_cells
-    # Checks if .cells returns a Hash object
+    # Confirms .cells returns a Hash object
     assert_instance_of Hash, @board.cells
-    # Checks if Hash has 16 key-value pairs
+
+    # Confirms Hash has 16 key-value pairs
     assert_equal 16, @board.cells.size
-    # Checks if the class of every key is String, and every value is Cell
+
+    # Confirms the class of every key is String, and every value is Cell
     assert_equal true, @board.cells.keys.all? {|key| key.class == String}
     assert_equal true, @board.cells.values.all? {|value| value.class == Cell}
-    # Checks if each key has a length of 2
+    
+    # Confirms each key has a length of 2
     assert_equal 2, @board.cells.keys.sample.length
   end
 
   def test_it_can_identify_a_valid_coordinate
+    # Confirms returns true if coordinate exists as a key in hash of cells
     assert_equal true, @board.valid_coordinate?("A4")
     assert_equal true, @board.valid_coordinate?("B2")
+
+    # Confirms returns false if it doesn't
     assert_equal false, @board.valid_coordinate?("A5")
     assert_equal false, @board.valid_coordinate?("A22")
     assert_equal false, @board.valid_coordinate?("D5")
   end
 
-  def test_
+  def test_it_can_check_valid_placement_of_ship
+    # Confirms return value is false if length of placement is different than length of ship
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"]) # Too short
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A3", "A4"]) # Too long
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"]) # Too long
+    assert_equal false, @board.valid_placement?(@submarine, ["A2"]) # Too short
+
+    # Confirms return value is false if placement cells are non-consecutive
+
+  end
+
+  def
 end
