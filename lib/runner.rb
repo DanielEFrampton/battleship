c7b69d0f0ab0113c47446277c28c5f127c0d0180
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-15 16:03:10 -0600

    Define main_menu method
    Co-Authored-By: Ana Perez Santos <aperezsantos@users.noreply.github.com>

--- lib/runner.rb
+++ lib/runner.rb
@@ -5,7 +5,26 @@ class Runner
     @game = nil
   end
 
+  def main_menu
+    puts "Welcome to BATTLESHIP"
+    loop do
+      puts "Enter p to play. Enter q to quit."
+      user_input = gets.chomp
+      if user_input == 'p'
+        setup # proceed to next step of game
+      elsif user_input == 'q'
+        break # ends the main_menu method, closing the Ruby file
+      end
+    end
+  end
+
+  def setup
+  end
+
   # def convert_to_array(input)
   #   input.split(' ')
   # end
 end
+
+runner = Runner.new
+runner.main_menu
