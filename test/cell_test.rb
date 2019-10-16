77f0bd6c8cfa57cef3df8e832bd8afed1afa2b1b
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-16 16:02:50 -0600

    Rearrange cell test to match order of methods in cell class

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -27,17 +27,14 @@ class CellTest < Minitest::Test
     assert_nil @cell_2.ship
   end
 
+  def test_fired_upon_starts_false
+    assert_equal false, @cell.fired_upon?
+    assert_equal false, @cell_2.fired_upon?
+  end
+
   def test_empty_returns_true_initially
     assert_equal true, @cell.empty?
     assert_equal true, @cell_2.empty?
-  end
-
-  def test_it_has_ship_after_placing_ship
-    @cell.place_ship(@cruiser)
-    assert_equal @cruiser, @cell.ship
-
-    @cell_2.place_ship(@submarine)
-    assert_equal @submarine, @cell_2.ship
   end
 
   def test_it_is_not_empty_after_placing_ship
@@ -50,22 +47,24 @@ class CellTest < Minitest::Test
     assert_equal false, @cell_2.empty?
   end
 
-  def test_fired_upon_starts_false
-    assert_equal false, @cell.fired_upon?
-    assert_equal false, @cell_2.fired_upon?
-  end
+  def test_it_has_ship_after_placing_ship
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
 
-  def test_fire_upon_lowers_ship_health_by_one
-    @cell.place_ship(@submarine)
-    @cell.fire_upon
-
-    assert_equal 1, @cell.ship.health
+    @cell_2.place_ship(@submarine)
+    assert_equal @submarine, @cell_2.ship
   end
 
   def test_cell_has_not_been_fired_upon
     assert_equal false, @cell.fired_upon?
     @cell.fire_upon
     assert_equal true, @cell.fired_upon?
+  end
+
+  def test_fire_upon_lowers_ship_health_by_one
+    @cell.place_ship(@submarine)
+    @cell.fire_upon
+    assert_equal 1, @cell.ship.health
   end
 
   def test_fire_upon_reduces_health
